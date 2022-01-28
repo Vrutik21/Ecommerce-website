da65f739fa5dd19f23243dbf2ec9a00e07a15024
Vrutik Parmar <parmarvrutik1@gmail.com>
2022-01-28 11:27:54 +0530

    commit on master

--- Database/DBController.php
+++ Database/DBController.php
@@ -15,15 +15,16 @@ class DBController
     public function __construct()
     {
         $this->con=mysqli_connect($this->host,$this->user,$this->password,$this->database);
+    //  check connection
         if($this->con->connect_error){
-            echo "Fail".$this->con->connect_error;
+            echo "CONNECTION FAILED: ".$this->con->connect_error;
         }
              
     }
     public function __destruct(){
         $this->closeConnection();
     }
-    
+//    closing connection
     protected function closeConnection(){
         if($this->con!=null){
             $this->con->close();
